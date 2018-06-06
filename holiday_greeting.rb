15b2b312ae05f9544bcef3332308e2c5f10214d9
randihi <randihigashi@gmail.com>
2018-06-06 05:07:02 +0000

    automatically backed up by learn

--- holiday_greeting.rb
+++ holiday_greeting.rb
@@ -1,2 +1,14 @@
 def holiday_greeting(to, from, holiday)
-  puts"To: #{to} From: #{from} Happy #{holiday}!"
+  puts "To: #{to} From: #{from} Happy #{holiday}!"
+end
+
+puts "Welcome! What's your name?"
+name = gets.chomp
+
+puts "Who would you like to send the card to?"
+send = gets.chomp
+
+puts "For what holiday?"
+holiday_name = gets.chomp
+
+puts holiday_greeting(send, name, holiday_name)
